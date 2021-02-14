c941fbc49dc1ea829b888a151464db2fe76586cf
Jonathan Sedrez <jonathansedrez@gmail.com>
2021-02-14 19:25:51 -0300

    fix tests

--- src/api/test/transactions.mock.ts
+++ src/api/test/transactions.mock.ts
@@ -5,7 +5,7 @@ export const mock: Transaction[] = [
     id: '5f89f9f257fe42957bf6dbfd',
     title: 'Resgate',
     description: 'et labore proident aute nulla',
-    status: 'processing',
+    status: 'created',
     amount: 2078.66,
     date: '2020-07-01',
     from: 'Aposentadoria',
